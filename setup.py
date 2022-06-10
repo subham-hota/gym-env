e45c091466c7383f55a097d82dc4e35a0de33e86
Subham Hota <shota@iitk.ac.in>
2022-06-10 18:28:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #Structuring code into a python package
 from setuptools import setup
 
-setup(name='gym_env',
+setup(name='gymenv',
     version='0.0.1',
     install_requires=['gym==0.23.1','pygame==2.1.0'])
